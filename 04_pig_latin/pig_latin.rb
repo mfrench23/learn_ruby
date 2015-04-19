92d8f0ac57f29cbde2bdf7774b1a59ecff156f7e
Mitchell French <mitch.french@gmail.com>
2015-04-18 18:08:59 -0700

    Improve logic of 04_pig_latin solution
    Better regex allows removal of ugly hack for 'qu' special case

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,7 @@
 def translate(phrase)
   phrase.split(/ /).each do |word|
-    squash_qu word
     move_consonants_to_end word
     add_ay word
-    expand_qu word
   end.join(' ')
 end
 
@@ -12,17 +10,5 @@ def add_ay(word)
 end
 
 def move_consonants_to_end(word)
-  word.gsub!(/^([^aeiou]*)(.*)$/, '\2\1')
+  word.gsub!(/^(([bcdfghjklmnprstvwxyz]|qu)+)(.*)$/, '\3\1')
 end
-
-def squash_qu(word)
-  # Text being converted to pig latin is unlikely
-  # to include the infinity symbol...
-  word.gsub!(/qu/, 'Ꝏ')
-end
-
-def expand_qu(word)
-  word.gsub!(/Ꝏ/, 'qu')
-end
-
-
